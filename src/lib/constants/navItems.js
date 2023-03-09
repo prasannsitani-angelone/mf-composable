8171880488a8f3cee1c46e435632ebb1e9ae5f8c
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2023-03-09 12:28:55 +0530

    Remove SGB ref

--- src/lib/constants/navItems.js
+++ src/lib/constants/navItems.js
@@ -4,17 +4,18 @@ import HoldingActiveIcon from '$lib/images/icons/HoldingActiveIcon.svelte';
 import HoldingIcon from '$lib/images/icons/HoldingIcon.svelte';
 import OrdersBottomNavbarActiveIcon from '$lib/images/icons/OrdersBottomNavbarActiveIcon.svelte';
 import OrdersBottomNavbarInactiveIcon from '$lib/images/icons/OrdersBottomNavbarInactiveIcon.svelte';
+import { base } from '$app/paths';
 
 export const BOTTOM_NAVBARS = [
 	{
-		label: 'Gold Bonds',
-		path: '/goldBonds',
+		label: 'Discover',
+		path: `${base}/discoverfunds`,
 		icon: HomeIcon,
 		activeIcon: HomeActiveIcon
 	},
 	{
-		label: 'Holdings',
-		path: '/holdings',
+		label: 'Investments',
+		path: `${base}/investments`,
 		icon: HoldingIcon,
 		activeIcon: HoldingActiveIcon
 	},
--- src/lib/stores/PageStore.js
+++ src/lib/stores/PageStore.js
@@ -3,6 +3,6 @@ import { writable } from 'svelte/store';
 export const page = writable({
 	showBottomNavigation: true,
 	showSearchIcon: true,
-	title: 'Gold Bonds',
+	title: 'Mutual funds',
 	showBackIcon: false
 });
