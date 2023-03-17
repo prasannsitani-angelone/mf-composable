2ee85946efd5791ed25bebd669fc9e14d9a08f59
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2023-03-17 18:17:08 +0530

    Update +page.ts

--- src/routes/(app)/discoverfunds/+page.ts
+++ src/routes/(app)/discoverfunds/+page.ts
@@ -5,19 +5,17 @@ import type { DiscoverFund } from '$lib/types/IDiscoverFunds';
 import type { PageLoad } from './$types';
 
 export const load = (async ({ fetch, parent }) => {
-	const parentData = await parent();
-	console.log(parentData);
 	const url = `${PUBLIC_MF_CORE_BASE_URL}/schemes/searchDashboard?options=true`;
 	const headers = {
 		userType: `${profileStore.userType()}`,
 		accountType: `${profileStore?.accountType()}`,
 		authorization: `Bearer ${tokenStore.activeToken()}`
 	};
-	console.log(headers);
 	const res = await fetch(url, {
 		headers
 	});
-	console.log('----------------------------------------------------------------------');
+	console.log(res);
+
 	if (res.ok) {
 		const discoverFundData = await res.json();
 		return {
