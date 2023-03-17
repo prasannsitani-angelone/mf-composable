294e8deb276bac85ef5d1f5a489c068a4f31c1ce
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2023-03-17 18:14:11 +0530

    Move page load from server to universal

---
+++ src/routes/(app)/discoverfunds/+page.ts
@@ -0,0 +1,29 @@
+import { PUBLIC_MF_CORE_BASE_URL } from '$env/static/public';
+import { profileStore } from '$lib/stores/ProfileStore';
+import { tokenStore } from '$lib/stores/TokenStore';
+import type { DiscoverFund } from '$lib/types/IDiscoverFunds';
+import type { PageLoad } from './$types';
+
+export const load = (async ({ fetch, parent }) => {
+	const parentData = await parent();
+	console.log(parentData);
+	const url = `${PUBLIC_MF_CORE_BASE_URL}/schemes/searchDashboard?options=true`;
+	const headers = {
+		userType: `${profileStore.userType()}`,
+		accountType: `${profileStore?.accountType()}`,
+		authorization: `Bearer ${tokenStore.activeToken()}`
+	};
+	console.log(headers);
+	const res = await fetch(url, {
+		headers
+	});
+	console.log('----------------------------------------------------------------------');
+	if (res.ok) {
+		const discoverFundData = await res.json();
+		return {
+			homePage: discoverFundData
+		};
+	} else {
+		return {};
+	}
+}) satisfies PageLoad;
