fbe73dcce6a8aff91e454f28d81e5904bef1531a
BhagatAO <123355556+BhagatAO@users.noreply.github.com>
2023-04-27 17:58:50 +0530

    Navigate sip l2 (#84)
    * fix: naviaget to nuxt for L2
    
    * fix: scheme card navigate to mutual-funds v1
    
    * fix: click of sip card fixed
    
    * fix: skip sip nudge
    
    * fix: skip sip nudge
    
    * fix: binded the value for skip sip text
    
    * fix: for cache
    
    * fix: for cache

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -57,7 +57,9 @@ const handler = (async ({ event, resolve }) => {
 	};
 
 	const response = await resolve(event);
-	response.headers.set('Cache-Control', 'no-cache');
+	response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+	response.headers.set('Pragma', 'no-cache');
+	response.headers.set('Expires', '0');
 	return response;
 }) satisfies Handle;
 
