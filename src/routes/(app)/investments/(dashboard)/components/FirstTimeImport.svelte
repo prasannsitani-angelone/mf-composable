ccdc87cfdda3387e5f2d7ece94eff4bc0b54890e
Nitish Bhaskar <124981423+nitish-nix@users.noreply.github.com>
2023-05-09 14:18:52 +0530

    WMS-3798: Corrected font weight (#133)
    WMS-3796: Corrected text transform

--- src/routes/(app)/investments/(dashboard)/components/FirstTimeImport.svelte
+++ src/routes/(app)/investments/(dashboard)/components/FirstTimeImport.svelte
@@ -12,9 +12,11 @@
 			View all your investments in one place by tracking external investments with Angel One
 		</div>
 		<img src={FirstTimeImportSVG} loading="lazy" alt="Illustration shoeing first time import" />
-		<Button class="mt-8 w-full px-2" onClick={onConfirmation}>Track External Investments</Button>
+		<Button class="mt-8 w-full px-2 text-sm !font-light" onClick={onConfirmation}
+			>TRACK EXTERNAL INVESTMENTS</Button
+		>
 
-		<div class="mt-2 text-xs text-grey-body">
+		<div class="mt-2 text-xs text-black">
 			This will not affect your external investments in any way
 		</div>
 	</div>
