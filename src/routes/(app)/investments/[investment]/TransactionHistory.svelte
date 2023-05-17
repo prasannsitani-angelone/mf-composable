6e25d93660df499ae8741798a41e92408ebd09ed
Nitish Bhaskar <124981423+nitish-nix@users.noreply.github.com>
2023-05-17 14:55:34 +0530

    Feature/wms 3994 fixes (#216)
    * Last refreshed to be always displayed on external funds investment details page
    
    * Fixed the upcoming orders not showing on investments page
    
    * WMS-3994
    
    * CTA changes as per WMS-3994

--- src/routes/(app)/investments/[investment]/TransactionHistory.svelte
+++ src/routes/(app)/investments/[investment]/TransactionHistory.svelte
@@ -2,6 +2,8 @@
 	import Card from '$components/Card.svelte';
 	import TransactionHistorytable from './components/TransactionHistorytable.svelte';
 	import type { OrdersEntity } from '$lib/types/IInvestments';
+	import { page } from '$app/stores';
+	import { WMSIcon } from 'wms-ui-component';
 
 	export let transactionList: OrdersEntity[] = [];
 
@@ -19,6 +21,7 @@
 			modifiedTransactionList = transactionList?.slice(0, tableListThreshold) || [];
 		}
 	};
+	$: isExternal = $page?.data?.isExternal;
 </script>
 
 <section class={$$props.class}>
@@ -35,13 +38,27 @@
 			</div>
 		{/if}
 
-		{#if transactionList?.length && transactionList?.length > 5}
+		{#if transactionList?.length && transactionList?.length > 5 && !isExternal}
 			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<div
 				class="flex cursor-pointer items-end justify-center py-5 text-sm font-semibold text-blue-primary"
 				on:click={toggleShowFullTransactionList}
 			>
 				View {showFullTransactionList ? 'Less' : 'All'}
+			</div>
+		{:else if transactionList?.length && transactionList?.length > 5 && isExternal}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div
+				class="flex cursor-pointer items-center justify-center py-5 text-sm font-semibold text-blue-primary"
+				on:click={toggleShowFullTransactionList}
+			>
+				{#if !showFullTransactionList}
+					<div>SHOW MORE</div>
+					<div><WMSIcon width={14} height={7} name="arrow-expand" class="ml-2" /></div>
+				{:else}
+					<div>SHOW LESS</div>
+					<div><WMSIcon width={14} height={7} name="arrow-collapse" class="ml-2" /></div>
+				{/if}
 			</div>
 		{/if}
 	</Card>
