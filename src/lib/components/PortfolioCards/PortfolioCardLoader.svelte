78a26bf15bb2fc0beb9d20ca7b74c3dd59c3f3b9
Nitish Bhaskar <124981423+nitish-nix@users.noreply.github.com>
2023-05-25 20:24:00 +0530

    Added Table skeletan Loader (#296)

--- src/lib/components/PortfolioCards/PortfolioCardLoader.svelte
+++ src/lib/components/PortfolioCards/PortfolioCardLoader.svelte
@@ -1,12 +1,34 @@
 <script>
 	import SkeletonRectangle from '$components/Skeleton/SkeletonRectangle.svelte';
 	import SkeletonWrapper from '$components/Skeleton/SkeletonWrapper.svelte';
-	// import {page} from "$app/store"
 	let discoverPage = false;
-	// const isMobile = $page.data.deviceType
 	export { discoverPage };
 </script>
 
-<SkeletonWrapper>
-	<SkeletonRectangle class={`mb-2 w-full rounded-lg ${discoverPage ? '!h-24' : '!h-64'}`} />
+<SkeletonWrapper class=" w-full rounded-lg bg-white p-6 shadow-csm max-sm:p-4 max-sm:pb-0 ">
+	<div class="  bg-white pb-2">
+		<div class="mb-2 flex">
+			<SkeletonRectangle class="h-2 flex-1" />
+			<SkeletonRectangle class="ml-8 h-2 flex-1" />
+		</div>
+		<div class="flex">
+			<SkeletonRectangle class="h-5 flex-1" />
+			<SkeletonRectangle class="ml-8 h-5 flex-1" />
+		</div>
+		<SkeletonRectangle class="mt-6 !h-16 w-full {discoverPage ? 'max-sm:hidden' : ''}" />
+	</div>
+	<div
+		class="mx-auto mt-2 flex w-full justify-center rounded-md border border-grey-line px-3 py-5 max-sm:hidden"
+	>
+		<SkeletonRectangle class="h-5 w-[60%] " />
+	</div>
+
+	<div
+		class=" mt-2 flex border-t border-grey-line bg-white pt-2 pb-4 sm:hidden {!discoverPage
+			? 'pt-4'
+			: ''}"
+	>
+		<SkeletonRectangle class="h-2 flex-1 " />
+		<SkeletonRectangle class="ml-8 h-2 flex-1 {discoverPage ? 'flex' : 'hidden'}" />
+	</div>
 </SkeletonWrapper>
--- src/routes/(app)/(authenticated)/investments/(dashboard)/Loaders/InvestmentDashboardLoader.svelte
+++ src/routes/(app)/(authenticated)/investments/(dashboard)/Loaders/InvestmentDashboardLoader.svelte
@@ -1,8 +1,104 @@
 <script>
 	import SkeletonRectangle from '$components/Skeleton/SkeletonRectangle.svelte';
 	import SkeletonWrapper from '$components/Skeleton/SkeletonWrapper.svelte';
+	import SkeletonCircle from '$components/Skeleton/SkeletonCircle.svelte';
 </script>
 
 <SkeletonWrapper>
-	<SkeletonRectangle class="mb-2 !h-[80vh] w-full rounded-lg" />
+	<!-- <SkeletonRectangle class="mb-2 !h-[80vh] w-full rounded-lg" /> -->
+	<!-- <FundDetailsLoader /> -->
+	<SkeletonWrapper class="border-1 rounded-lg bg-white py-3 md:mt-5">
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<SkeletonRectangle class=" mb-2 w-[50%]" />
+			<SkeletonRectangle class="mb-2 ml-4 w-[15%] flex-1" />
+			<SkeletonRectangle class="mb-2 ml-4 w-[15%] flex-1" />
+			<SkeletonRectangle class="mb-2 ml-4 w-[10%] flex-1" />
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+		<div class="flex h-full flex-row space-x-5 border-b border-grey-line px-4">
+			<div class=" mt-4 flex w-[60%] gap-3">
+				<SkeletonCircle class="h-9 w-9" />
+				<div class="w-[50%]">
+					<SkeletonRectangle class="mb-2 w-2/4" />
+					<SkeletonRectangle class="mb-2 w-full" />
+				</div>
+			</div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+			<div class="mt-4 flex w-[15%] gap-3"><SkeletonRectangle class="mb-2 w-full" /></div>
+		</div>
+	</SkeletonWrapper>
+
+	<!-- <OrderpadLoader /> -->
 </SkeletonWrapper>
