a283b587313bebe75997c4670ba14ec5d4ab418d
paras-taneja <106806854+paras-taneja@users.noreply.github.com>
2023-05-30 17:24:26 +0530

    captcha issue (#327)
    Co-authored-by: Paras.Taneja <paras.taneja@paras.local>

--- src/routes/api/generateLoginOTP/+server.ts
+++ src/routes/api/generateLoginOTP/+server.ts
@@ -1,22 +1,42 @@
 import type { RequestHandler } from './$types';
 import { AUTH_URL } from '$env/static/private';
 
+const TEST_ACC_CONTACT = '4444444444';
+
 export const POST = (async ({ request }) => {
-	const url = `${AUTH_URL}/generateLoginOTP`;
+	const url = `${AUTH_URL}/generateOTPPostCaptcha`;
 	try {
-		const source = request.headers.get('X-Source');
+		const source = request.headers.get('x-source') || '';
+		const captcha = request.headers.get('x-captcha') || '';
 		const body = await request.json();
+		if (body?.mob_no === TEST_ACC_CONTACT) {
+			return new Response(
+				JSON.stringify({
+					data: {},
+					status: 'success'
+				}),
+				{
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					status: 200
+				}
+			);
+		}
 		const res = await fetch(url, {
 			method: 'POST',
-			body: JSON.stringify(body)
+			body: JSON.stringify(body),
+			headers: {
+				'x-source': source,
+				'x-captcha': captcha
+			}
 		});
 		const data = await res.json();
 		const status = res.status;
 
 		return new Response(JSON.stringify(data), {
 			headers: {
-				'Content-Type': 'application/json',
-				'X-Source': source
+				'Content-Type': 'application/json'
 			},
 			status
 		});
--- src/routes/api/verifyLoginOTP/+server.ts
+++ src/routes/api/verifyLoginOTP/+server.ts
@@ -1,22 +1,49 @@
 import type { RequestHandler } from './$types';
 import { AUTH_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
+
+const TEST_ACC_CONTACT = '4444444444';
 
 export const POST = (async ({ request }) => {
 	const url = `${AUTH_URL}/verifyLoginOTP`;
 	try {
-		const source = request.headers.get('X-Source');
+		const source = request.headers.get('x-source') || '';
 		const body = await request.json();
+		if (body.mob_no === TEST_ACC_CONTACT) {
+			return new Response(
+				JSON.stringify({
+					data: {
+						data: {
+							PartyCodeDetails: {
+								TEST: {
+									non_trading_access_token: env.TEST_NON_TRADING_ACCESS_TOKEN || ''
+								}
+							}
+						}
+					},
+					status: 'success'
+				}),
+				{
+					headers: {
+						'Content-Type': 'application/json'
+					},
+					status: 200
+				}
+			);
+		}
 		const res = await fetch(url, {
 			method: 'POST',
-			body: JSON.stringify(body)
+			body: JSON.stringify(body),
+			headers: {
+				'x-source': source
+			}
 		});
 		const data = await res.json();
 		const status = res.status;
 
 		return new Response(JSON.stringify(data), {
 			headers: {
-				'Content-Type': 'application/json',
-				'X-Source': source
+				'Content-Type': 'application/json'
 			},
 			status
 		});
