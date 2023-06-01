41f5bb3f93f73040e79c0e7f7c0a44674b25f68a
Nitish Bhaskar <124981423+nitish-nix@users.noreply.github.com>
2023-06-01 14:07:54 +0530

    Fixed loader for Angel BEe (#352)

--- src/lib/components/OverlayLoading.svelte
+++ src/lib/components/OverlayLoading.svelte
@@ -3,6 +3,6 @@
 	import Overlay from './Overlay.svelte';
 </script>
 
-<Overlay class=" justify-center">
+<Overlay class=" justify-center" containerClass="!justify-center sm:!justify-center">
 	<LoadingIndicator svgClass="!w-16 !h-16" />
 </Overlay>
