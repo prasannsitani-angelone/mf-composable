2c2b933b7d62afece2fb5b0555528c5725da4f5c
BhagatAO <123355556+BhagatAO@users.noreply.github.com>
2023-06-22 12:19:22 +0530

    Wms 4627 (#492)
    * fix: redirect to login from switch confirmation
    
    * fix: comment resolved

--- src/routes/(app)/schemes/switch/confirm/+page.ts
+++ src/routes/(app)/schemes/switch/confirm/+page.ts
@@ -1,7 +1,13 @@
+import { browser } from '$app/environment';
+import { goto } from '$app/navigation';
+import { base } from '$app/paths';
 import { decodeToObject } from '$lib/utils/helpers/params';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
-export const load = (async ({ url }) => {
+export const load = (async ({ url, parent }) => {
+	const parentData = await parent();
+	const { pathname, search } = url;
 	const params = url.searchParams.get('params');
 	const decodedParams = decodeToObject(params || undefined);
 	const {
@@ -13,6 +19,11 @@ export const load = (async ({ url }) => {
 		switchInFund,
 		fullAmountSelected
 	} = decodedParams;
+	if (!parentData?.tokenObj?.userToken?.NTAccessToken && pathname) {
+		const withRedirectParam = `${base}/login?redirect=${encodeURIComponent(pathname + search)}`;
+		if (browser) return await goto(withRedirectParam);
+		else throw redirect(302, withRedirectParam);
+	}
 	return {
 		layoutConfig: {
 			title: 'Confirm Switch',
