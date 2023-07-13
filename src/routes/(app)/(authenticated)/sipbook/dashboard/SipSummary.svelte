70ffe3c051e21cbe1cc0b43d3d83264b4b200d8f
codx-mayank <106961814+codx-mayank@users.noreply.github.com>
2023-07-13 10:43:26 +0530

    WMS-4860 bug fixes (WMS-4895 WMS-4896 WMS-4899 WMS-4900) (#587)

--- src/routes/(app)/(authenticated)/sipbook/dashboard/SipSummary.svelte
+++ src/routes/(app)/(authenticated)/sipbook/dashboard/SipSummary.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import RightIcon from '$lib/images/icons/RightIcon.svelte';
+	import { deviceStore } from '$lib/stores/DeviceStore';
 	import type { ISipBookOverView } from '$lib/types/ISipType';
 	import { addCommasToAmountString } from '$lib/utils/helpers/formatAmount';
 
 	let bookSummary: ISipBookOverView;
 	let automatedSipsCount = 0;
 	export { bookSummary, automatedSipsCount };
+
+	const os = $page?.data?.deviceType?.osName || $page?.data?.deviceType?.os;
+	const userAgent = $deviceStore.userAgent?.toLowerCase();
 
 	const handleAutopayButtonClick = () => {
 		// based on automatedSipsCount, redirect to setup autopay journey or autopay list page
@@ -18,7 +23,10 @@
 </script>
 
 <section
-	class="relative mb-2 rounded-lg bg-gradient-to-r from-blue-gradient via-blue-gradient to-blue-primary pt-6 pb-3 text-white md:mt-0 lg:p-6"
+	class="relative mb-2 rounded-lg bg-gradient-to-r from-blue-gradient via-blue-gradient to-blue-primary pt-6 pb-3 text-white md:mt-0 lg:p-6 {os ===
+		'iOS' || userAgent?.includes('safari')
+		? 'overflow-hidden'
+		: ''}"
 >
 	<div
 		class={`absolute left-[34%] -top-56 h-[26rem] w-20 rotate-[-30deg] rounded-lg bg-gradient-to-b opacity-20 bg-blend-screen mix-blend-screen`}
@@ -36,15 +44,20 @@
 
 		<section class="flex-1 text-center">
 			<div class="text-xs font-normal">Monthly SIP Total</div>
-			<div class="text-lg font-medium">
-				₹{addCommasToAmountString(bookSummary?.totalSipInstallmentAmount?.toFixed(0))}
+			<div class="flex items-center justify-center text-lg font-medium">
+				<div class="text-sm">₹</div>
+				<div class="ml-0.5">
+					{addCommasToAmountString(bookSummary?.totalSipInstallmentAmount?.toFixed(0))}
+				</div>
 			</div>
 		</section>
 	</article>
 
 	<article class="flex items-center justify-between px-4 pt-3">
 		<div class="text-xs font-medium">
-			Automated SIPs - <span>{automatedSipsCount}</span>
+			Automated SIPs - <span class={automatedSipsCount ? 'text-white' : 'text-red-errorDark'}
+				>{automatedSipsCount}</span
+			>
 		</div>
 
 		<button class="flex items-center" on:click={handleAutopayButtonClick}>
