5a708226ff48abb8dbc805fdccbb956165e105fc
prashant-angelbroking <76934125+prashant-angelbroking@users.noreply.github.com>
2023-09-11 22:15:42 +0530

    Update AutopaySetupTile layout for responsive design (#789)

--- src/lib/components/AutopaySetupTile/OrdersAutoPayComponent.svelte
+++ src/lib/components/AutopaySetupTile/OrdersAutoPayComponent.svelte
@@ -13,21 +13,23 @@
 </script>
 
 <article
-	class="mb-3 mt-1 flex max-w-4xl flex-col items-center rounded-lg bg-white p-4 py-6 shadow-csm {$$props.class}"
+	class="mb-3 mt-1 flex w-full flex-col items-center rounded-lg bg-white p-4 py-6 shadow-csm sm:flex-row sm:justify-center {$$props.class}"
 >
-	<UPIMandateIcon class="h-[124px] w-[165px]" />
+	<UPIMandateIcon class="h-[124px] w-[165px] sm:mr-10 sm:h-[195px] sm:w-[258px]" />
 
-	{#if amount <= UPI_MANDATE_MAX_AMOUNT}
-		<p class="mb-2 mt-6 text-sm font-semibold text-black-title">Set Up UPI Autopay</p>
+	<div class="flex flex-col items-center">
+		{#if amount <= UPI_MANDATE_MAX_AMOUNT}
+			<p class="mb-2 mt-6 text-sm font-semibold text-black-title">Set Up UPI Autopay</p>
 
-		<p class="mb-4 text-xs text-black-title">Automate your SIP payments in 2 simple steps</p>
+			<p class="mb-4 text-xs text-black-title">Automate your SIP payments in 2 simple steps</p>
 
-		<Button onClick={autoPayClick} class="w-full">SET UP UPI AUTOPAY</Button>
-	{:else}
-		<p class="mb-2 mt-6 text-sm font-semibold text-black-title">Set Up Autopay Now</p>
+			<Button onClick={autoPayClick} class="w-full sm:w-[328px]">SET UP UPI AUTOPAY</Button>
+		{:else}
+			<p class="mb-2 mt-6 text-sm font-semibold text-black-title">Set Up Autopay Now</p>
 
-		<p class="mb-4 text-xs text-black-title">Make SIP payments automatically with Autopay</p>
+			<p class="mb-4 text-xs text-black-title">Make SIP payments automatically with Autopay</p>
 
-		<Button onClick={autoPayClick} class="w-full">SET UP AUTOPAY</Button>
-	{/if}
+			<Button onClick={autoPayClick} class="w-full sm:w-[328px]">SET UP AUTOPAY</Button>
+		{/if}
+	</div>
 </article>
