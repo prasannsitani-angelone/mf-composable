ad715df4cc1f9c1da5e90f14227e37bf85145320
prashant-angelbroking <76934125+prashant-angelbroking@users.noreply.github.com>
2023-09-14 13:55:31 +0530

    Wms 5525 upi auto pay   stand alone flow (entry point and auto pay creation) (#797)
    * setting default payment mode only if previous payment mode not set
    
    * Remove AutopayIllustration component, using UPIMandateIcon instead
    
    * Improve data handling in autopay management page. Previously, Card was shown when the total amount is above 0. This commit adds a check to ensure that response.data exist
    
    * Removed the condition if unlinked amount is zero or less.

--- src/routes/(app)/(authenticated)/autopay/manage/(autopay)/+page.svelte
+++ src/routes/(app)/(authenticated)/autopay/manage/(autopay)/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-	import Button from '$components/Button.svelte';
-	import { base } from '$app/paths';
-	import { goto } from '$app/navigation';
 	import Card from '$components/Card.svelte';
 	import RiskMessage from './components/RiskMessage.svelte';
 	import InfoList from './components/InfoList.svelte';
@@ -15,42 +12,24 @@
 	export let data;
 	const params = $page.url.searchParams.get('params');
 	const { showAlert = false } = decodeToObject(params || '');
-
-	const navigateToAutopayDashboard = () => {
-		goto(`${base}/autopay`);
-	};
 </script>
 
 {#await data.api.data}
 	<ManageSkeletanLoader />
 {:then response}
 	{@const { totalAmount } = getSipAmountWithoutMandate(response?.data)}
-	{#if totalAmount > 0}
-		<Card class="px-2 pb-6 pt-4">
-			{#if showAlert}
-				<RiskMessage {totalAmount} />
-			{/if}
-			<div class="my-16 flex justify-center">
-				<UPIMandateIcon class=" h-[200px] w-[265px]" />
-			</div>
-			<section class=" m-auto text-center text-2xl font-medium text-black-title">
-				<div>Automate your SIP</div>
-				<div>Payments with Autopay</div>
-			</section>
-			<InfoList />
-			<BankDetails {totalAmount} />
-		</Card>
-	{:else}
-		<Card
-			><div class="flex flex-col items-center justify-center text-lg font-medium">
-				<div class=" text-center">
-					You already have existing autopay setup from your account.<br /><br /> You can still manage
-					your autopay.
-				</div>
-				<Button class="mt-8" variant="outlined" onClick={navigateToAutopayDashboard}
-					>Manage Autopay</Button
-				>
-			</div></Card
-		>
-	{/if}
+	<Card class="px-2 pb-6 pt-4">
+		{#if showAlert}
+			<RiskMessage {totalAmount} />
+		{/if}
+		<div class="my-16 flex justify-center">
+			<UPIMandateIcon class=" h-[200px] w-[265px]" />
+		</div>
+		<section class=" m-auto text-center text-2xl font-medium text-black-title">
+			<div>Automate your SIP</div>
+			<div>Payments with Autopay</div>
+		</section>
+		<InfoList />
+		<BankDetails {totalAmount} />
+	</Card>
 {/await}
