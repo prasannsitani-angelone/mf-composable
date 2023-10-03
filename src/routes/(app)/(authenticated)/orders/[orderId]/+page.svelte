a6c298a93213c20dd35209d714d9c7289c5d7a5a
codx-mayank <106961814+codx-mayank@users.noreply.github.com>
2023-10-03 12:48:23 +0530

    WMS-5797 order details deep link loader issue fix (#835)
    * WMS-5797 order details deep link loader issue fix
    
    * WMS-5797 data fetching loader issue fix

--- src/routes/(app)/(authenticated)/orders/[orderId]/+page.svelte
+++ src/routes/(app)/(authenticated)/orders/[orderId]/+page.svelte
@@ -17,6 +17,11 @@
 	import RightArrowIcon from './RightArrowIcon.svelte';
 	import { encodeObject } from '$lib/utils/helpers/params';
 	import { needHelpClickAnalytics } from '$lib/analytics/orders/orders';
+	import { afterUpdate, onMount } from 'svelte';
+
+	let isPageMounted = false;
+	let isDataFetched = false;
+
 	$: breadCrumbs = [
 		{
 			text: 'Home',
@@ -48,6 +53,18 @@
 	};
 	// const icon = <WMSIcon name="order-details-error"/>
 	export let data: PageData;
+
+	onMount(() => {
+		isPageMounted = true;
+	});
+
+	afterUpdate(() => {
+		if (isPageMounted && data?.api?.getOrdersData?.data?.orderId) {
+			isDataFetched = true;
+		} else {
+			isDataFetched = false;
+		}
+	});
 </script>
 
 <article class="flex flex-col">
@@ -56,64 +73,79 @@
 		seoDescription="Get your order status details with one click including transaction date, id, charges etc."
 	/>
 	<Breadcrumbs items={breadCrumbs} class="mb-4 hidden items-center justify-start md:flex" />
-	{#await data?.api?.getOrdersData}
+	{#if !isPageMounted || !isDataFetched}
 		<OrderDetailLoader />
-	{:then ordersData}
-		{#if ordersData.data}
-			<div class="pb-5" class:!pb-20={ordersData.showFooterButton}>
-				<Header
-					heading={ordersData?.headerContent?.heading}
-					title={data.layoutConfig.title}
-					subHeadingText={ordersData?.headerContent?.subHeadingText}
-					subHeaderClass={ordersData?.headerContent?.subHeaderClass}
-					status={ordersData?.headerContent?.status}
-					class="border-b border-grey-line sm:border-b-0"
-				/>
-				<div class="mt-3 px-2 md:px-0">
-					<OrderCard orderDetails={ordersData.data} showStatusNote={ordersData.showStatusNote} />
-					<div class="mt-3 rounded-lg bg-white px-3 py-3">
-						<div
-							class="mb-5 text-sm font-medium text-black-title"
-							class:!text-base={ordersData?.data?.investmentType?.toUpperCase() === 'REDEEM'}
-						>
-							{ordersData?.data?.investmentType?.toUpperCase() === 'REDEEM'
-								? 'Withdrawal Timeline'
-								: 'Order Timeline'}
+	{:else}
+		{#await data?.api?.getOrdersData}
+			<OrderDetailLoader />
+		{:then ordersData}
+			{#if ordersData.data}
+				<div class="pb-5" class:!pb-20={ordersData.showFooterButton}>
+					<Header
+						heading={ordersData?.headerContent?.heading}
+						title={data.layoutConfig.title}
+						subHeadingText={ordersData?.headerContent?.subHeadingText}
+						subHeaderClass={ordersData?.headerContent?.subHeaderClass}
+						status={ordersData?.headerContent?.status}
+						class="border-b border-grey-line sm:border-b-0"
+					/>
+					<div class="mt-3 px-2 md:px-0">
+						<OrderCard orderDetails={ordersData.data} showStatusNote={ordersData.showStatusNote} />
+						<div class="mt-3 rounded-lg bg-white px-3 py-3">
+							<div
+								class="mb-5 text-sm font-medium text-black-title"
+								class:!text-base={ordersData?.data?.investmentType?.toUpperCase() === 'REDEEM'}
+							>
+								{ordersData?.data?.investmentType?.toUpperCase() === 'REDEEM'
+									? 'Withdrawal Timeline'
+									: 'Order Timeline'}
+							</div>
+							<OrderTimeLine items={ordersData?.orderStatusItems || []} />
 						</div>
-						<OrderTimeLine items={ordersData?.orderStatusItems || []} />
-					</div>
-					{#if ordersData?.data?.transactionType?.toUpperCase() === TRANSACTION_TYPE.PURCHASE}
-						<Button
-							color="white"
-							class="mt-2 !h-auto w-full !transform-none items-center justify-between !rounded-lg !px-3 !py-3 shadow-csm"
-							endAdornment={RightArrowIcon}
-							on:click={() => navigateToFAQ(ordersData?.tag, ordersData?.headerContent?.heading)}
-						>
-							<div class="flex items-center">
-								<WMSIcon name="question-mark-circle" class="mr-3" width={18} height={18} />
-								<div class="text-left">
-									<div class="font-medium capitalize">Need help ?</div>
-									<div class="text-xs font-normal capitalize text-grey-body">
-										Queries related to order status, charges and more
+						{#if ordersData?.data?.transactionType?.toUpperCase() === TRANSACTION_TYPE.PURCHASE}
+							<Button
+								color="white"
+								class="mt-2 !h-auto w-full !transform-none items-center justify-between !rounded-lg !px-3 !py-3 shadow-csm"
+								endAdornment={RightArrowIcon}
+								on:click={() => navigateToFAQ(ordersData?.tag, ordersData?.headerContent?.heading)}
+							>
+								<div class="flex items-center">
+									<WMSIcon name="question-mark-circle" class="mr-3" width={18} height={18} />
+									<div class="text-left">
+										<div class="font-medium capitalize">Need help ?</div>
+										<div class="text-xs font-normal capitalize text-grey-body">
+											Queries related to order status, charges and more
+										</div>
 									</div>
 								</div>
-							</div>
-						</Button>
+							</Button>
+						{/if}
+						<OrderTransactions
+							orderDetails={ordersData.data}
+							statusItems={ordersData?.statusItems || {}}
+						/>
+					</div>
+					{#if ordersData.showFooterButton}
+						<Footer
+							isOrderFailedAtExchange={ordersData?.isOrderFailedAtExchange}
+							schemeDetails={ordersData?.schemeDetails}
+							orderDetailsData={ordersData.data}
+						/>
 					{/if}
-					<OrderTransactions
-						orderDetails={ordersData.data}
-						statusItems={ordersData?.statusItems || {}}
-					/>
 				</div>
-				{#if ordersData.showFooterButton}
-					<Footer
-						isOrderFailedAtExchange={ordersData?.isOrderFailedAtExchange}
-						schemeDetails={ordersData?.schemeDetails}
-						orderDetailsData={ordersData.data}
-					/>
-				{/if}
-			</div>
-		{:else}
+			{:else}
+				<ErrorView
+					heading="Order Details Unavailable"
+					contentLine="The link you are trying to reach is not valid. Please go to orders to check your recent orders"
+					textForButton="GO TO ORDERS"
+					on:errorCTAClicked={handleErrorNavigation}
+				>
+					<svelte:fragment slot="icon">
+						<WMSIcon height={160} width={160} name="order-details-error" />
+					</svelte:fragment>
+				</ErrorView>
+			{/if}
+		{:catch}
 			<ErrorView
 				heading="Order Details Unavailable"
 				contentLine="The link you are trying to reach is not valid. Please go to orders to check your recent orders"
@@ -124,17 +156,6 @@
 					<WMSIcon height={160} width={160} name="order-details-error" />
 				</svelte:fragment>
 			</ErrorView>
-		{/if}
-	{:catch}
-		<ErrorView
-			heading="Order Details Unavailable"
-			contentLine="The link you are trying to reach is not valid. Please go to orders to check your recent orders"
-			textForButton="GO TO ORDERS"
-			on:errorCTAClicked={handleErrorNavigation}
-		>
-			<svelte:fragment slot="icon">
-				<WMSIcon height={160} width={160} name="order-details-error" />
-			</svelte:fragment>
-		</ErrorView>
-	{/await}
+		{/await}
+	{/if}
 </article>
