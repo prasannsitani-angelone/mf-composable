09bff2bb014c94cf06e2f752ca396afbbe1f7ca3
prashant-angelbroking <76934125+prashant-angelbroking@users.noreply.github.com>
2023-12-22 11:27:05 +0530

    Tax analysis copy changes (#1109)

--- src/routes/(app)/(authenticated)/investments/portfolio/components/TaxAnalysis.svelte
+++ src/routes/(app)/(authenticated)/investments/portfolio/components/TaxAnalysis.svelte
@@ -21,6 +21,7 @@
 		fillColor: '#019C81' | '#FACE80';
 		taxType: 'STCG' | 'LTCG';
 	}
+
 	const investmentTypes: IInvestmentTypes[] = [
 		{
 			label: 'Short term investment',
@@ -37,6 +38,7 @@
 			taxType: 'LTCG'
 		}
 	];
+
 	function getCurrentFiscalYear() {
 		const today = new Date();
 		const curMonth = today.getMonth();
@@ -135,13 +137,13 @@
 			<TaxationIcon />
 		</div>
 		<div class="flex flex-col">
-			<h2 class="text-base text-black-key">Tax Analysis</h2>
+			<h2 class="text-base text-black-key">Tax Overview</h2>
 			<h3 class="text-xs text-black-bolder">Your short term and long term gains</h3>
 		</div>
 	</header>
 	<section class="flex flex-col border-b border-grey-line p-4">
-		<h4 class="mb-3 text-sm font-medium text-black-key">Tax Capital Gains</h4>
-		<p class="text-xs text-black-bolder">Fund split by holding period</p>
+		<h4 class="mb-3 text-sm font-medium text-black-key">Capital Gains Tax Split</h4>
+		<p class="text-xs text-black-bolder">Applicable on your portfolio if you withdraw today</p>
 		<!-- For Graph -->
 		<div class="mt-3">
 			<LinearChart chartInput={taxGainGraph} />
@@ -182,35 +184,43 @@
 		</div>
 	</section>
 
-	{#if taxationData?.totalElssInvestedFy < taxationData?.elssInvestmentCap}
-		<section class="flex flex-col p-4">
-			<h4 class="mb-3 text-sm font-medium text-black-key">Save Taxes with ELSS Funds</h4>
+	<section class="flex flex-col p-4">
+		<h4 class="mb-3 text-sm font-medium text-black-key">Save Taxes with ELSS Funds</h4>
 
-			<p class="mb-4 text-xs text-black-bolder">
+		<p class="mb-4 text-xs text-black-bolder">
+			{#if taxationData?.totalElssInvestedFy >= taxationData?.elssInvestmentCap}
+				You have already invested
+				<AmountText amount={taxationData?.elssInvestmentCap?.toFixed()} />
+				in ELSS funds and availed tax benefits for financial year {getCurrentFiscalYear().start}
+				to ‘{getCurrentFiscalYear().end}
+			{:else}
 				Invest up to
 				<AmountText amount={taxationData?.maxElssInvestAllowed?.toFixed()} />
-				more in ELSS funds till March {getCurrentFiscalYear().end}
-				to reduce your taxable income by <strong>₹1.5 Lakh </strong> under
-				<strong>section 80C</strong> of the Income Tax Act of India
-			</p>
+				more in ELSS funds till March {getCurrentFiscalYear().end} to reduce your taxable income by
+				<span class="font-bold">₹1.5 Lakh</span>
+				under
+				<span class="font-bold">section 80C</span> of the Income Tax Act of India
+			{/if}
+		</p>
 
-			<div class="flex">
-				<div class="flex flex-col">
-					<p class="text-xs text-black-bolder">Invested</p>
-					<p class="text-sm font-medium text-black-key">
-						<AmountText amount={taxationData?.totalElssInvestedFy?.toFixed()} />
-					</p>
-				</div>
-				<div class="ml-auto flex flex-col">
-					<p class="text-right text-xs text-black-bolder">Limit</p>
-					<p class="text-sm font-medium text-black-key">
-						<AmountText amount={taxationData?.elssInvestmentCap?.toFixed()} />
-					</p>
-				</div>
+		<div class="flex">
+			<div class="flex flex-col">
+				<p class="text-xs text-black-bolder">Invested</p>
+				<p class="text-sm font-medium text-black-key">
+					<AmountText amount={taxationData?.totalElssInvestedFy?.toFixed()} />
+				</p>
 			</div>
-			<div class="mt-3">
-				<LinearChart chartInput={elssInvestmetnGraph} />
+			<div class="ml-auto flex flex-col">
+				<p class="text-right text-xs text-black-bolder">Limit</p>
+				<p class="text-sm font-medium text-black-key">
+					<AmountText amount={taxationData?.elssInvestmentCap?.toFixed()} />
+				</p>
 			</div>
+		</div>
+		<div class="mt-3">
+			<LinearChart chartInput={elssInvestmetnGraph} />
+		</div>
+		{#if taxationData?.totalElssInvestedFy < taxationData?.elssInvestmentCap}
 			<div class="flex items-center justify-between md:my-2">
 				<h4 class="text-sm font-medium text-black-key">Explore Tax Saving Funds</h4>
 				<Button
@@ -222,6 +232,6 @@
 					EXPLORE NOW
 				</Button>
 			</div>
-		</section>
-	{/if}
+		{/if}
+	</section>
 </article>
