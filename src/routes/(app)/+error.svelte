00538543e189697c37c453556e242d1664935d6b
codx-mayank <106961814+codx-mayank@users.noreply.github.com>
2024-01-12 14:25:08 +0530

    WMS-7312 WMS-7188 app error handling changes (#1182)

--- src/routes/(app)/+error.svelte
+++ src/routes/(app)/+error.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import SomethingWentWrong from '$components/Error/SomethingWentWrong.svelte';
 	import MobileHeader from '$components/Headers/MobileHeader.svelte';
-	import BottomNavigation from '$components/BottomNavigation.svelte';
-	import { BOTTOM_NAVBARS } from '$lib/constants/navItems';
 
-	let version = '';
+	const reloadPageOnRetry = () => {
+		if (navigator?.onLine) {
+			window?.location?.reload();
+		}
+	};
 </script>
 
 <article class="-mx-2 h-[calc(100vh-80px)] bg-white">
@@ -18,10 +20,6 @@
 		/>
 	</section>
 	<section class="flex h-full flex-col items-center justify-center pb-12 md:pb-0">
-		<SomethingWentWrong showPageLayout={true} />
+		<SomethingWentWrong showPageLayout={true} on:retryButtonClick={reloadPageOnRetry} />
 	</section>
 </article>
-
-<section class="fixed -mx-2 -mt-12 w-full md:hidden">
-	<BottomNavigation navs={BOTTOM_NAVBARS(version)} />
-</section>
