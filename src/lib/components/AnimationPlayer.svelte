b12b65c47346a8b3c201200a9717c4430520c2a7
vishal-angle-one <143476122+vishal-angle-one@users.noreply.github.com>
2024-01-31 16:26:25 +0530

    Order success animation (#1244)
    * SIP Assist - Multiple Baskets payment flow
    
    * review changes
    
    * Animation added
    
    * Review changes
    
    * Review changes

---
+++ src/lib/components/AnimationPlayer.svelte
@@ -0,0 +1,99 @@
+<script>
+	import { onMount } from 'svelte';
+
+	export let bannerText = '';
+	export let AnimatedLottie = '';
+
+	let LottiePlayer;
+
+	onMount(async () => {
+		const module = await import('@lottiefiles/svelte-lottie-player');
+		LottiePlayer = module.LottiePlayer;
+	});
+
+	let controlsLayout = [
+		'previousFrame',
+		'playpause',
+		'stop',
+		'nextFrame',
+		'progress',
+		'frame',
+		'spacer',
+		'background',
+		'snapshot',
+		'zoom',
+		'info'
+	];
+</script>
+
+{#if LottiePlayer}
+	<section class="flex h-[95vh] w-full flex-col items-center justify-center">
+		<LottiePlayer
+			src={AnimatedLottie}
+			autoplay={true}
+			loop={false}
+			controls={false}
+			renderer="svg"
+			background="transparent"
+			height={100}
+			width={100}
+			{controlsLayout}
+		/>
+		<div id="banner" class="fade-in-text mt-3 text-lg font-medium">{bannerText}</div>
+	</section>
+{/if}
+
+<style>
+	.fade-in-text {
+		animation: fadeIn 5s;
+		-webkit-animation: fadeIn 5s;
+		-moz-animation: fadeIn 5s;
+		-o-animation: fadeIn 5s;
+		-ms-animation: fadeIn 5s;
+	}
+
+	@keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	@-moz-keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	@-webkit-keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	@-o-keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+	@-ms-keyframes fadeIn {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+</style>
