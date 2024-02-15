db5a2d021c16cbca709c725d7facc4538dc49a85
paras-taneja <106806854+paras-taneja@users.noreply.github.com>
2024-02-15 14:43:33 +0530

    New ia search component fix (#1327)
    * prod issue for hindi language selected
    
    * filters growth issue
    
    * enhancing refreshToken experience
    
    * bug fix for ia
    
    * search component changed for new IA
    
    ---------
    
    Co-authored-by: Paras.Taneja <paras.taneja@paras.local>
    Co-authored-by: vishal-angle-one <143476122+vishal-angle-one@users.noreply.github.com>

--- src/routes/(app)/discoverfunds/SearchComponent.svelte
+++ src/routes/(app)/discoverfunds/SearchComponent.svelte
@@ -1,30 +1,8 @@
 <script lang="ts">
-	import TextInput from '$components/TextInput.svelte';
 	import SearchDarkIcon from '$lib/images/icons/SearchDarkIcon.svelte';
-
-	let classes = {
-		container: 'py-3 mr-1 w-full border-none rounded-md',
-		label: '',
-		input:
-			'w-full !border-none focus:outline-none placeholder-body md:placeholder-disabled !text-base !text-title font-normal lg:font-normal !input-xs !pl-0',
-		error: '',
-		parent: 'w-full'
-	};
 </script>
 
-<article class="mt-2 rounded-3xl bg-white">
-	<section class={`relative flex flex-col items-center justify-center md:px-3`}>
-		<!-- Search Input -->
-		<div class="flex w-full cursor-text items-center rounded-md border-0 lg:w-[440px]">
-			<article class="flex w-full items-center">
-				<SearchDarkIcon class="mx-4 h-6 w-6 text-title" />
-				<TextInput
-					id="searchInput"
-					name="searchInput"
-					placeholder="Search by fund name, type or AMC"
-					{classes}
-				/>
-			</article>
-		</div>
-	</section>
+<article class="mt-2 flex items-center rounded-3xl border border-border bg-background-alt">
+	<SearchDarkIcon class="mx-4 h-5 w-6 text-disabled" />
+	<div class="py-3 text-base text-disabled">Search by fund name, type or AMC</div>
 </article>
