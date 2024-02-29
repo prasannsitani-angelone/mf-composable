fabf8097d2989d5877008d86ead6535c62fedb6d
deepti-mathur <100206122+deepti-mathur@users.noreply.github.com>
2024-02-29 11:05:39 +0530

    WMS-8261 - removed ICICI video (#1418)

--- src/lib/components/Stories/utils.ts
+++ src/lib/components/Stories/utils.ts
@@ -14,20 +14,6 @@ export const videoCtaList: Array<videoCtaUrls> = [
 		}
 	},
 	{
-		videoId: 3,
-		ctaList: {
-			B2C_D:
-				'/schemes/icici-prudential-value-discovery-fund-direct-plan-growth-isin-inf109k012k1-schemecode-8176-gr',
-			B2C_P:
-				'/schemes/icici-prudential-value-discovery-fund-direct-plan-growth-isin-inf109k012k1-schemecode-8176-gr',
-			B2B_D:
-				'/schemes/icici-prudential-value-discovery-fund-growth-isin-inf109k01af8-schemecode-dfg',
-			B2B_P:
-				'/schemes/icici-prudential-value-discovery-fund-growth-isin-inf109k01af8-schemecode-dfg',
-			genericUrl: ''
-		}
-	},
-	{
 		videoId: 4,
 		ctaList: {
 			B2C_D:
@@ -87,22 +73,6 @@ export const storiesDataObjectWithoutUrls: StoriesData = {
 			],
 			imageThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/thumbnails/thumbnail2c2.png',
 			smallThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/smallThumbnails/thumbnail2c3.png',
-			ctaType: '',
-			ctaText: 'START SIP NOW',
-			ctaUrl: ''
-		},
-		{
-			storyId: 3,
-			title: 'Best of ICICI',
-			videos: [
-				{
-					videoId: 3,
-					videoUrl: '/videos/video3c5/playlist.m3u8',
-					videoFallbackUrl: 'https://cdn.angelone.in/mutualfunds/videos/video3c5.mp4'
-				}
-			],
-			imageThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/thumbnails/thumbnail3c2.jpg',
-			smallThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/smallThumbnails/thumbnail3c3.jpg',
 			ctaType: '',
 			ctaText: 'START SIP NOW',
 			ctaUrl: ''
@@ -176,22 +146,6 @@ export const setStoriesData = (getCtaUrlCallback: (vidId: number) => string) => 
 				ctaType: '',
 				ctaText: 'START SIP NOW',
 				ctaUrl: getCtaUrlCallback(2)
-			},
-			{
-				storyId: 3,
-				title: 'Best of ICICI',
-				videos: [
-					{
-						videoId: 3,
-						videoUrl: '/videos/video3c5/playlist.m3u8',
-						videoFallbackUrl: 'https://cdn.angelone.in/mutualfunds/videos/video3c5.mp4'
-					}
-				],
-				imageThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/thumbnails/thumbnail3c2.jpg',
-				smallThumbnailUrl: 'https://cdn.angelone.in/mutualfunds/smallThumbnails/thumbnail3c3.jpg',
-				ctaType: '',
-				ctaText: 'START SIP NOW',
-				ctaUrl: getCtaUrlCallback(3)
 			},
 			{
 				storyId: 4,
