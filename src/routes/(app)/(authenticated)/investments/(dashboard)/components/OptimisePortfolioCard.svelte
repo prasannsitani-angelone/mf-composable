70deef77ca00b4851d9d078129c13b240ead8057
deepti-mathur <100206122+deepti-mathur@users.noreply.github.com>
2024-03-12 17:38:41 +0530

    WMS-8462 - dark mode fixes (#1501)

--- src/routes/(app)/(authenticated)/investments/(dashboard)/components/OptimisePortfolioCard.svelte
+++ src/routes/(app)/(authenticated)/investments/(dashboard)/components/OptimisePortfolioCard.svelte
@@ -12,25 +12,25 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div on:click on:keydown class={$$props.class}>
 	<section
-		class="flex items-center justify-between rounded-lg bg-white shadow-lg hover:cursor-pointer lg:mx-0"
+		class="flex items-center justify-between rounded-lg bg-background-alt shadow-lg hover:cursor-pointer lg:mx-0"
 	>
 		<article class="flex flex-col items-start px-4 py-3">
-			<div class="text-sm font-medium">
+			<div class="text-sm font-medium text-title">
 				{addCommasToAmountString(peopleInvested)} other investors started a 2nd SIP to maximise their
 				returns
 			</div>
 			<div class="mt-2 flex text-xs font-normal">
 				<div class="flex flex-col items-start justify-center pr-2">
-					<p>Explore 2nd SIP based on your 1st investment</p>
+					<p class="text-body">Explore 2nd SIP based on your 1st investment</p>
 					<ButtonMedium
 						size={BtnSize.SM}
-						class="!bg-background-alt !p-0 !text-xs !font-medium !text-blue-primary"
-						on:click>EXPLORE NOW<WMSIcon name="right-arrow" stroke="#3F5BD9" /></ButtonMedium
+						class="!bg-background-alt !p-0 !text-xs !font-medium !text-primary"
+						on:click>EXPLORE NOW<WMSIcon name="right-arrow" stroke="var(--PRIMARY)" /></ButtonMedium
 					>
 				</div>
 				<div class="flex items-center justify-center">
 					<div
-						class="flex h-16 w-16 flex-col items-center rounded-sm border border-tint24-primary text-2xs text-black-key"
+						class="flex h-16 w-16 flex-col items-center rounded-sm border border-tint24-primary text-2xs text-title"
 					>
 						<div class="flex h-2/3 items-center justify-center">
 							<SchemeLogo
@@ -45,13 +45,15 @@
 						</div>
 					</div>
 					<div class="mx-2">
-						<WMSIcon name="plus-no-bordericon" stroke="#ACB2BD" height={14} width={14} />
+						<WMSIcon name="plus-no-bordericon" stroke="var(--DISABLED)" height={14} width={14} />
 					</div>
 					<div
-						class="flex h-16 w-16 flex-col items-center rounded-sm border border-tint24-primary text-2xs text-black-key"
+						class="flex h-16 w-16 flex-col items-center rounded-sm border border-tint24-primary text-2xs text-title"
 					>
-						<div class="flex h-2/3 items-center justify-center text-2xl text-blue-primary">?</div>
-						<div class="flex h-1/3 w-full items-center justify-center bg-purple-glow align-bottom">
+						<div class="flex h-2/3 items-center justify-center text-2xl text-primary">?</div>
+						<div
+							class="flex h-1/3 w-full items-center justify-center bg-tint12-secondary-alt align-bottom"
+						>
 							2nd SIP
 						</div>
 					</div>
