8dc70f60ab1fd5a20041cde4282527204b3afa88
Bhaskar gyan vardhan <bhaskarGyan@users.noreply.github.com>
2024-03-13 10:14:31 +0530

    WMS-8426-web-perf-reduce-initial-document-size (#1485)
    * Seperate style from head
    
    * comment hls js
    
    * Update app.html

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
 			base: '/mutual-funds'
 		},
 		alias: { 'lib/': './src/routes/lib/', $components: './src/lib/components' },
-		inlineStyleThreshold: 300000
+		inlineStyleThreshold: 1024
 	}
 };
 
