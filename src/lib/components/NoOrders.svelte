e56a0beea5acf1177b67696ebd32390975e85aae
deepti-mathur <100206122+deepti-mathur@users.noreply.github.com>
2024-03-13 18:33:58 +0530

    empty state changes (#1519)

--- src/lib/components/NoOrders.svelte
+++ src/lib/components/NoOrders.svelte
@@ -7,8 +7,8 @@
 	class="flex flex-col items-center rounded-lg bg-background-alt p-4 text-title shadow-csm lg:py-10 {$$props.class}"
 >
 	<NoOrdersImage class="m-3" />
-	<section class="mt-6 flex flex-col items-center">
-		<div class="text-base font-normal">
+	<section class="mt-6 flex w-64 flex-col items-center text-center text-body">
+		<div class="text-sm font-normal">
 			{title}
 		</div>
 	</section>
--- src/routes/(app)/(authenticated)/orders/orderspage/OrdersDashboard/NoOrdersPage.svelte
+++ src/routes/(app)/(authenticated)/orders/orderspage/OrdersDashboard/NoOrdersPage.svelte
@@ -4,32 +4,14 @@
 	import RightIcon from '$lib/images/icons/RightIcon.svelte';
 	import MostBought from '$components/MostBought/MostBought.svelte';
 	import type { PageData } from '../../../../../$types';
-	import OrderFilter from './OrderFilter/OrderFilter.svelte';
-	import type { OrdersSummary } from '$lib/types/IInvestments';
-	import { onMount } from 'svelte';
-	import { filterStore } from '$lib/stores/FilterStore';
 	let data: PageData;
 	const classes = {
 		header: 'p-4',
 		container: `!pb-0 ${$$props.class}`
 	};
 	export { data };
-	const ordersSummary: OrdersSummary = {
-		totalFailedOrders: 0,
-		totalProcessingOrders: 0,
-		totalScheduledOrders: 0,
-		totalCompletedOrders: 0
-	};
-
-	onMount(() => {
-		const filters = { failed: true, completed: true, inprogress: true };
-		filterStore.updateStore(filters);
-	});
 </script>
 
-<article class="block md:hidden">
-	<OrderFilter {ordersSummary} />
-</article>
 <article class="hidden md:block">
 	<NoOrders title="You do not have any orders currently. Explore funds and place an order" />
 </article>
